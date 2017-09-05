cb845d85e595ae671dfd3c646b3ba41478acf991
Corey Sader <corey.sader@gmail.com>
2017-09-05 10:49:46 -0500

    part 7 tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,24 @@ import { EmployeeComponent } from './employee/employee.component'
   selector: 'app-root',
   template: `
     <div>
-      <h1>{{pageHeader}}</h1>
+      <!-- Interpolation -->
+      <h1>{{'Page Header = ' + pageHeader}}</h1>
+      <!-- ternary operator -->
+      <h2>{{subHeader ? subHeader : 'There is no Sub-Header'}}</h2>
+      <img src="{{imagePath}}"/>
       <employee></employee>
+      <p>{{getFullName()}}</p>
     </div>
   `,
 })
 export class AppComponent {
   pageHeader: string = 'Employee Details';
+  subHeader: string = '';
+  imagePath: string = 'http://www.pragimtech.com/Images/Logo.JPG';
+  firstName: string = 'Corey';
+  lastName: string = 'Sader';
+
+  getFullName() : string {
+    return 'This application created by: ' + this.firstName + ' ' + this.lastName;
+  }
 }
