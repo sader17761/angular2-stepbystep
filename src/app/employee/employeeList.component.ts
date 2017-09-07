16ee03bce2439bbcaf2a88e89cb873c8760142de
Corey Sader <corey.sader@gmail.com>
2017-09-07 14:47:41 -0500

    part 17 tutorial

--- src/app/employee/employeeList.component.ts
+++ src/app/employee/employeeList.component.ts
@@ -7,10 +7,28 @@ import { Component } from '@angular/core';
 })
 
 export class EmployeeListComponent {
-  employees: any[] = [
-    { code: 'emp101', name: 'Tom', gender: 'Male', annualSalary: 5500, dateOfBirth: '25/6/1988'},
-    { code: 'emp102', name: 'Alex', gender: 'Male', annualSalary: 5700.95, dateOfBirth: '9/6/1982'},
-    { code: 'emp103', name: 'Mike', gender: 'Male', annualSalary: 5900, dateOfBirth: '12/8/1979'},
-    { code: 'emp104', name: 'Mary', gender: 'Female', annualSalary: 6500.826, dateOfBirth: '14/10/1980'},
-  ];
+  employees: any[];
+
+  constructor() {
+    this.employees = [
+      { code: 'emp101', name: 'Tom', gender: 'Male', annualSalary: 5500, dateOfBirth: '6/25/1988'},
+      { code: 'emp102', name: 'Alex', gender: 'Male', annualSalary: 5700.95, dateOfBirth: '9/6/1982'},
+      { code: 'emp103', name: 'Mike', gender: 'Male', annualSalary: 5900, dateOfBirth: '12/8/1979'},
+      { code: 'emp104', name: 'Mary', gender: 'Female', annualSalary: 6500.826, dateOfBirth: '10/14/1980'},
+    ];
+  }
+
+  getEmployees(): void {
+    this.employees = [
+      { code: 'emp101', name: 'Tom', gender: 'Male', annualSalary: 5500, dateOfBirth: '6/25/1988'},
+      { code: 'emp102', name: 'Alex', gender: 'Male', annualSalary: 5700.95, dateOfBirth: '9/6/1982'},
+      { code: 'emp103', name: 'Mike', gender: 'Male', annualSalary: 5900, dateOfBirth: '12/8/1979'},
+      { code: 'emp104', name: 'Mary', gender: 'Female', annualSalary: 6500.826, dateOfBirth: '10/14/1980'},
+      { code: 'emp105', name: 'Nancy', gender: 'Female', annualSalary: 6500.826, dateOfBirth: '12/15/1982'},
+    ];
+  }
+
+  trackByEmpCode(index: number, employee: any): string{
+    return employee.code;
+  }
 }
