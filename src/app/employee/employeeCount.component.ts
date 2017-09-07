0a6e5225f911b0d08546dc1dcaf14f703234dd08
Corey Sader <corey.sader@gmail.com>
2017-09-07 16:13:23 -0500

    part 20 tutorial

---
+++ src/app/employee/employeeCount.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'employee-count',
+  templateUrl: './employeeCount.component.html',
+  styleUrls: ['./employeeCount.component.css',]
+})
+
+export class EmployeeCountComponent {
+  all: number = 10;
+  male: number = 5;
+  female: number = 5;
+}
